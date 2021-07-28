77892a76502f82d1eeca0fdc029162d06143c573
JK2K <ikuba03@gmail.com>
2021-07-28 16:13:52 +0200

    feat(node): add alternavte node init

--- List/node.h
+++ List/node.h
@@ -1,15 +1,15 @@
 #ifndef RATTLE_DATA_TYPES_NODE_H
 #define RATTLE_DATA_TYPES_NODE_H
-typedef struct NODE_STRUCT Node;
-struct Node {
+typedef struct NODE_STRUCT {
     void* data;
-    struct Node* next;
-};
+    struct NODE_STRUCT* next;
+} Node;
 
 Node* node_init(void* data);
+Node* node_init_with_next(void* data, Node* n);
 
-Node* node_next(Node* node);
+Node* node_next(Node* n);
 
 //Returns pointer to data required to be free separately due to unknown type
-void* node_free(Node* node);
+void* node_free(Node* n);
 #endif //RATTLE_DATA_TYPES_NODE_H
