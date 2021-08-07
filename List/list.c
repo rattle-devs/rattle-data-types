2a2a90ccef8f5b11d8bb43ae4401241d0232f2d8
JK2K <ikuba03@gmail.com>
2021-08-07 19:17:55 +0200

    feat(node): some implemtatiobs

--- List/list.c
+++ List/list.c
@@ -1,1 +1,71 @@
+#include <malloc.h>
 #include "list.h"
+
+List* list_init(Node* head, Node* tail){
+    List* l = (List*)malloc(sizeof(List));
+    if(l != NULL){
+        l->head = head;
+        if(tail != NULL){
+            l->tail = tail;
+            tail->next = head;
+        } else{
+            l->tail = head;
+        }
+        return l;
+    }
+    return l;
+}
+
+void list_add_tail(List* l, Node* node){
+    if(l->tail == NULL){
+        l->tail = node;
+        l->head = node;
+    } else{
+        node->next = l->tail;
+        l->tail = node;
+    }
+}
+
+void list_add_head(List* l, Node* node){
+    if(l->head == NULL){
+        l->head = node;
+        if(l->tail == NULL){
+            l->tail = node;
+        } else {
+            l->tail->next = node;
+        }
+    } else {
+        l->head->next = node;
+        l->head = node;
+    }
+}
+
+Node* list_pop_tail(List* l){
+    Node* tmp = l->tail;
+    if(tmp != NULL)
+        l->tail = tmp->next;
+    else
+        l->head = NULL;
+    return tmp;
+}
+
+Node* list_head(List* l){
+    return l->head;
+}
+
+Node* list_tail(List* l){
+    return l->tail;
+}
+
+bool list_empty(List* l){
+    return l->tail == NULL;
+}
+
+//if list is empty free it and returns True else just returns False
+bool list_free(List* l){
+    if(l->tail == NULL && l->head == NULL){
+        free(l);
+        return true;
+    }
+    return false;
+}
--- Queue/queue.c
+++ Queue/queue.c
@@ -1,1 +1,47 @@
+#include <malloc.h>
 #include "queue.h"
+
+Queue *queue_init(Node *node) {
+    Queue *q = (Queue *) malloc(sizeof(Queue));
+    if (q != NULL) {
+        q->head = node;
+        q->tail = node;
+        return q;
+    }
+    return q;
+}
+
+void queue_enqueue(Queue *q, Node *node) {
+    if (node == NULL)
+        return;
+
+    if (q->tail == NULL) {
+        q->head = node;
+        q->tail = node;
+    } else {
+        q->tail->next = node;
+        q->tail = node;
+    }
+}
+
+Node *queue_dequeue(Queue *q) {
+    Node *tmp = q->head;
+    if (tmp != NULL) {
+        q->head = tmp->next;
+        if (q->head == NULL)
+            q->tail = NULL;
+    }
+    return tmp;
+}
+
+bool queue_empty(Queue *q) {
+    return q->tail == NULL;
+}
+
+bool queue_free(Queue *q){
+    if (q->tail == NULL && q->head == NULL){
+        free(q);
+        return true;
+    }
+    return false;
+}
